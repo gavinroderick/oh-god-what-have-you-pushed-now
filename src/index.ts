b2ed894c1eb2609dd7569de1c7e75de84c816758
Gavin Roderick <gavin.roderick26@gmail.com>
2022-07-15 21:25:14 +0100

    feat: add webhook validator class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { WebhookValidator } from "./WebhookValidator";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
+
+app.get("/ping", (req: Request, res: Response) => {
+  res.sendStatus(204);
+});
+
+app.post("/new-push", (req: Request, res: Response) => {
+  const githubSignature = req.header("X-Hub-Signature-256");
+  if (githubSignature == undefined)
+    res.status(400).send("Could not find the header 'X-Hub-Signature-256'");
+
+  if (!WebhookValidator.SecretValid(req.body, githubSignature!))
+    res.status(400).send("Could not validate request");
+
+  res.sendStatus(200);
 });
